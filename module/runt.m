8ea59a14acd5d28a0fc30b79da986f7061e47ff1
caerwynj <caerwynj@localhost>
2007-02-08 22:50:17 +0000

    cleanup

--- module/runt.m
+++ module/runt.m
@@ -2,10 +2,6 @@ implement nothing;
 
 include "sys.m";
 include "draw.m";
-include "prefab.m";
-include "tk.m";
 include "math.m";
 include "keyring.m";
 include "loader.m";
-# include "readimage.m";
-include "freetype.m";
