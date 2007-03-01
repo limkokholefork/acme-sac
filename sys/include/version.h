1757d1d994d9a918564c6859cf1d672509e33c3e
caerwynj <caerwynj@localhost>
2007-03-01 03:09:57 +0000

    more pruning

--- sys/include/version.h
+++ sys/include/version.h
@@ -1,1 +1,1 @@
-#define VERSION	"Fourth Edition/acme-sac (20061222)"
+#define VERSION	"Fourth Edition/acme-sac (20070210)"
--- sys/libinterp/load.c
+++ sys/libinterp/load.c
@@ -627,10 +627,7 @@ unload(Module *m)
 		last = &mm->link;
 	}
 
-	if(m->rt == DYNMOD)
-		freedyncode(m);
-	else
-		destroy(m->origmp);
+	destroy(m->origmp);
 
 	destroylinks(m);
 
